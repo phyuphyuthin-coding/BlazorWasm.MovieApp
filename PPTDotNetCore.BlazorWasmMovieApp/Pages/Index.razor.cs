44c2314542b5fe62654e2f257cf9cf43e06f50a3
Phyu Phyu Thin <phyuthin2004@gmail.com>
2023-02-06 21:37:24 +0630

    Add project files.

---
+++ PPTDotNetCore.BlazorWasmMovieApp/Pages/Index.razor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using PPTDotNetCore.BlazorWasmMovieApp.Models;
+
+namespace PPTDotNetCore.BlazorWasmMovieApp.Pages
+{
+    public partial class Index
+    {
+        private MovieListResponseModel model;
+        private MovieDetailResponseModel movie;
+        private string searchTerm { get; set; }
+        private EnumFormCase _formCase = EnumFormCase.Search;
+        public async Task Enter(KeyboardEventArgs e)
+        {
+            try
+            {
+                if (e.Code == "Enter" || e.Code == "NumpadEnter")
+                {
+                    searchTerm =  await _JsRuntime.InvokeAsync<string>("onChangeText", new object[]{"movie-search-box"});
+                    // searchTerm = "Home";
+                    _formCase = EnumFormCase.Search;
+                    model = await _httpClient.GetFromJsonAsync<MovieListResponseModel>($"/?s={searchTerm}&page=1&apikey=fc1fef96");
+                    StateHasChanged();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                Console.WriteLine("No data!");
+            }
+        }
+
+        async Task Detail(string imdbID)
+        {
+            movie = await _httpClient.GetFromJsonAsync<MovieDetailResponseModel>($"/?i={imdbID}&apikey=fc1fef96");
+            _formCase = EnumFormCase.Detail;
+            StateHasChanged();
+        }
+        
+        public void hasFocus(FocusEventArgs args)
+        {
+            _formCase = EnumFormCase.Focus;
+             StateHasChanged();
+        }
+
+        public string CheckForm(EnumFormCase formCase)
+        {
+            switch (formCase)
+            {
+                case EnumFormCase.Detail:
+                    return "hide-search-list";
+                default:
+                case EnumFormCase.Focus:
+                case EnumFormCase.Search:
+                    return "";
+            }
+        }
+    }
+
+    public enum EnumFormCase
+    {
+        Search,
+        Detail,
+        Focus
+    }
+}
